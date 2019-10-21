373c204f3b9409ad81c9163701f35f83949fbd68
Jv <jvhaileapps@gmail.com>
2019-10-21 15:33:00 +0300

    Fixed error

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-  
 const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
-const { reply } = Telegraf;
+const {reply} = Telegraf;
 
 let ethiopic = require('ethiopic-js');
 
@@ -11,21 +10,28 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const welcomeMessage = "Hello";
 
 
-bot.start((ctx) => ctx.reply('Hello '));
+bot.start((ctx) => {
+    console.log("aa");
+    return ctx.reply('Hello ');
+});
 bot.help((ctx) => ctx.reply('Help message'));
 
-bot.command('date', (ctx)=>{
-  
-  let gDate = new Date();
-  
-  let eDate = ethiopic.toEthiopic(gDate.getFullYear(),gDate.getMonth() + 1,gDate.getDate());
-  
-  ctx.reply("In Ethiopian Calender to day is: " + eDate[2] + "/" + eDate[1] + "/" + eDate[0] );
-  
+bot.command('date', (ctx) => {
+    let gDate = new Date();
+    let eDate = ethiopic.toEthiopic(gDate.getFullYear(), gDate.getMonth() + 1, gDate.getDate());
+    return ctx.reply("In Ethiopian Calender to day is: " + eDate[2] + "/" + eDate[1] + "/" + eDate[0]);
 });
 
+const PRODUCTION = true;
+
+if (PRODUCTION) {
+    bot.telegram.setWebhook(`https://ethiopian-calender-bot.herokuapp.com/${process.env.BOT_TOKEN}`).then(console.log);
+    bot.startWebhook(`/${process.env.BOT_TOKEN}`, null, process.env.PORT);
+} else {
+    bot.launch()
+        .then(() => console.log("Bot Launched"))
+        .catch(console.log);
+}
 
 
-bot.launch();
 
-console.log(process.env.BOT_TOKEN);
